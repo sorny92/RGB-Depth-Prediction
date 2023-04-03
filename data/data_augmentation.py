f9762eccec327657afcfe09c406c7466b66da7cb
Esteve Soria <esofabian@gmail.com>
2023-04-03 11:03:17 +0200

    Less aggresive data augmentation

--- data/data_augmentation.py
+++ data/data_augmentation.py
@@ -1,19 +1,20 @@
 from albumentations import (
-    Compose, RandomBrightness, ImageCompression, HueSaturationValue, RandomBrightnessContrast, HorizontalFlip,
-    Rotate, CoarseDropout, VerticalFlip, PixelDropout, RandomResizedCrop
+    Compose, ImageCompression, HueSaturationValue, RandomBrightnessContrast, HorizontalFlip,
+    Rotate, CoarseDropout, VerticalFlip, PixelDropout
 )
 
 pixel_transforms = Compose([
     RandomBrightnessContrast(brightness_limit=0.2, contrast_limit=0.2, p=0.5),
-    HueSaturationValue(hue_shift_limit=20, sat_shift_limit=30, val_shift_limit=20, p=0.5),
-    CoarseDropout(),
+    HueSaturationValue(hue_shift_limit=10, sat_shift_limit=10, val_shift_limit=10, p=0.5),
+    CoarseDropout(max_holes=10, max_height=20, max_width=20,
+                  min_holes=1, min_height=5, min_width=5),
     PixelDropout(0.1),
-    ImageCompression(quality_lower=30, quality_upper=100, p=0.5),
+    ImageCompression(quality_lower=50, quality_upper=100, p=0.5),
 ])
 
 spatial_transforms = Compose([
-    RandomResizedCrop(512,512, scale=(0.6,1.0)),
-    Rotate(limit=180),
+    # RandomResizedCrop(512,512, scale=(0.6,1.0)),
+    Rotate(limit=90),
     HorizontalFlip(),
     VerticalFlip(),
 ], additional_targets={"depth": "image"})
