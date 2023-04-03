69df09f1667d0523d998ae7d7bcd52c936e6989c
Esteve Soria <esofabian@gmail.com>
2023-04-03 11:03:35 +0200

    Add image storage during training for debugging purposes

--- training.py
+++ training.py
@@ -1,5 +1,6 @@
 import os
 import pathlib
+import io
 
 import tensorflow as tf
 from tensorflow import keras
@@ -34,10 +35,65 @@ model_checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(
     mode='max',
     save_best_only=False)
 
-tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=f"./logs/{run_id}", update_freq=100, )
+log_dir = f"./logs/{run_id}"
+tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=log_dir, update_freq=100,
+                                                      profile_batch=5)
+file_writer_cm = tf.summary.create_file_writer(log_dir + '/cm', max_queue=1)
+
+
+def log_validation_images(epoch, logs):
+    import matplotlib.pyplot as plt
+    def plot_to_image(figure):
+        """Converts the matplotlib plot specified by 'figure' to a PNG image and
+        returns it. The supplied figure is closed and inaccessible after this call."""
+        # Save the plot to a PNG in memory.
+        buf = io.BytesIO()
+        figure.savefig(buf, format='png')
+        buf.seek(0)
+        # Convert PNG buffer to TF image
+        image = tf.image.decode_png(buf.getvalue(), channels=4)
+        # Add the batch dimension
+        image = tf.expand_dims(image, 0)
+        return image
+
+    def image_grid(input, prediction):
+        import numpy as np
+        # Create a figure to contain the plot.
+        figure = plt.figure(figsize=(20, 10))
+        for idx, i in enumerate([input[0][0], input[1][0], prediction]):
+            if i.dtype == np.float32:
+                i = np.copy(i)
+                i /= np.max(i)
+                i *= 255.0
+                i = i.astype(np.uint8)
+
+            # Start next subplot.
+            plt.subplot(1, 3, idx + 1)
+            plt.xticks([])
+            plt.yticks([])
+            plt.grid(False)
+            plt.imshow(i)
+            plt.tight_layout()
+        return figure
+
+    # Use the model to predict the values from the validation dataset.
+    print("HELLO")
+    in_image = val_dataset.take(1)
+    test_pred_raw = model.predict(in_image)[0]
+    input_image_as_np = in_image.as_numpy_iterator().next()
+
+    figure = image_grid(input_image_as_np, test_pred_raw)
+    cm_image = plot_to_image(figure)
+
+    with file_writer_cm.as_default():
+        res = tf.summary.image("epoch_validation_images", cm_image, step=epoch)
+
+
+# Define the per-epoch callback.
+images_callback = keras.callbacks.LambdaCallback(on_epoch_end=log_validation_images)
 
 reduce_lr_callback = tf.keras.callbacks.ReduceLROnPlateau(monitor='val_loss', factor=0.2,
-                                                          patience=5)
+                                                          patience=10)
 
 early_stopping_callback = tf.keras.callbacks.EarlyStopping(
     monitor="val_mean_absolute_error",
@@ -48,4 +104,5 @@ early_stopping_callback = tf.keras.callbacks.EarlyStopping(
 history = model.fit(train_dataset,
                     validation_data=val_dataset,
                     epochs=100,
-                    callbacks=[model_checkpoint_callback, tensorboard_callback, reduce_lr_callback])
+                    callbacks=[model_checkpoint_callback, tensorboard_callback, reduce_lr_callback,
+                               images_callback])
