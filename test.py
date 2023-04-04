fcef3c416035b1396f4705548adb757505b029e6
Esteve Soria <esofabian@gmail.com>
2023-04-05 00:23:21 +0200

    Implement delta threshold metric

--- test.py
+++ test.py
@@ -7,12 +7,33 @@ from training import custom_loss
 import argparse
 
 
+def ThresholdAccuracy(y_true, y_pred, threshold=1.25):
+    ranges = tf.maximum(y_true / y_pred, y_pred / y_true)
+    threshold = tf.cast(threshold, y_pred.dtype)
+    count = tf.cast(ranges < threshold, y_pred.dtype)
+    return tf.reduce_mean(count)
+
+
+def delta1(y_true, y_pred):
+    return ThresholdAccuracy(y_true, y_pred)
+
+
+def delta2(y_true, y_pred):
+    return ThresholdAccuracy(y_true, y_pred, threshold=1.25 * 1.25)
+
+
+def delta3(y_true, y_pred):
+    return ThresholdAccuracy(y_true, y_pred, threshold=1.25 * 1.25 * 1.25)
+
+
 def test(model_checkpoint, model, dataset_path):
     model.load_weights(model_checkpoint)
     model.compile(
         optimizer=keras.optimizers.Adam(learning_rate=0.001),
         loss=custom_loss,
-        metrics=[tf.keras.metrics.MeanAbsoluteError()],
+        metrics=[tf.keras.metrics.MeanAbsoluteError(),
+                 tf.keras.metrics.RootMeanSquaredError(),
+                 delta1, delta2, delta3],
     )
     _, test_dataset = create_train_val_dataset(dataset_path, aug_fn, 4, train_val_split=0)
 
